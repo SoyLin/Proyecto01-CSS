e1da00734d819251d84b0480051ffdaf1860c3d7
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-09-21 09:58:51 +0000

    Fixed link.

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -1,6 +1,5 @@
 console.log('Hola mundo')
 const contenedores = document.getElementById('contenedores')
-contenedores.onclick(link())
-function link(){
-    open('https://soylin.github.io/Proyecto01-CSS/Proyecto01/Contenedores/Contenedores.html')
-}
+contenedores.addEventListener('click',function (){
+    open('https://soylin.github.io/Proyecto01-CSS/Proyecto01/Contenedores/Contenedores.html');
+})
