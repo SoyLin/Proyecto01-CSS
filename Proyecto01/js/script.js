39021f0c5078b1c066704d603aff23803f52a8a4
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-09-21 10:01:14 +0000

    fixed null error

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -1,5 +1,5 @@
 console.log('Hola mundo')
 const contenedores = document.getElementById('contenedores')
-contenedores.addEventListener('click',function (){
+contenedores.addEventListener('click',function(){
     open('https://soylin.github.io/Proyecto01-CSS/Proyecto01/Contenedores/Contenedores.html');
 })
