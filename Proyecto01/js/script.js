57eb36aee9e8dd9e7cf27c4d60814259e83d4109
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-09-21 09:21:36 +0000

    Created: contenedores link

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -1,2 +1,6 @@
 console.log('Hola mundo')
 const contenedores = document.getElementById('contenedores')
+contenedores.onclick(link())
+function link(){
+    open('https://soylin.github.io/Proyecto01-CSS/Proyecto01/Contenedores/Contenedores.html')
+}
