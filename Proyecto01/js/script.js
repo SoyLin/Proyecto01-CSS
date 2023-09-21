e943b52f9043619399bbb72b782c26607d94508f
Victor <124076416+Vistaplay16@users.noreply.github.com>
2023-09-21 12:09:20 +0200

    arreglos con link

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -1,7 +1,5 @@
 console.log('Hola mundo')
 const contenedores = document.getElementById('contenedores')
-contenedores.addEventListener('click',link())
-
-function link(){
-    window.location.href('https://soylin.github.io/Proyecto01-CSS/Proyecto01/Contenedores/Contenedores.html')
-}
+contenedores.addEventListener("click", function(){
+    window.location.href="Contenedores.html"
+});
