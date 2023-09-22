8a31e63cf08e147ff8ec035163d54cbf6b506e97
Victor <124076416+Vistaplay16@users.noreply.github.com>
2023-09-22 13:27:17 +0200

    Ultimo arreglo en link

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -4,6 +4,3 @@ document.addEventListener('load', function() {
         window.location.href="../Proyecto01/contenedores/contenedores.html";
     });
 });
-window.addEventListener('click', function() {
-    window.location.href="../Proyecto01/contenedores/contenedores.html";
-});
