8d1ffce12574b10716d41344d3c83c3e244f4516
Victor <124076416+Vistaplay16@users.noreply.github.com>
2023-09-22 13:23:31 +0200

    arreglos en link

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -1,9 +1,9 @@
 document.addEventListener('load', function() {
     const contenedores = document.getElementById("contenedores");       
     contenedores.addEventListener("click", function(){
-        window.location.href="../contenedores/contenedores.html";
+        window.location.href="../Proyecto01/contenedores/contenedores.html";
     });
 });
 window.addEventListener('click', function() {
-    window.location.href="../contenedores/contenedores.html";
+    window.location.href="../Proyecto01/contenedores/contenedores.html";
 });
