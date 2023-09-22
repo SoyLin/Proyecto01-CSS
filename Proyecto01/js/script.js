0648e123264c07838ef88709ac93afe9278dcd3e
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-09-22 11:30:06 +0000

    fixing link again

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -1,6 +1,9 @@
 document.addEventListener('load', function() {
     const contenedores = document.getElementById("contenedores");       
-    contenedores.addEventListener("click", function(){
+    contenedores.addEventListener('click', function(){
         window.location.href="../Proyecto01/contenedores/contenedores.html";
     });
 });
+// window.addEventListener('click', function() {
+    // window.location.href="../Proyecto01/contenedores/contenedores.html";
+// });
