158b285c5aa9a2ab0e5fa89da3fd9e4b07c9c187
Vistaplay16 <victorferba@gmail.com>
2023-09-28 10:46:32 +0200

    nuevo link hacia formulario

--- Proyecto01/js/script.js
+++ Proyecto01/js/script.js
@@ -4,9 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
         window.location.href="../Proyecto01/contenedores/contenedores.html";
     });
 });
-// window.addEventListener('click', function() {
-    // window.location.href="../Proyecto01/contenedores/contenedores.html";
-// });
 document.addEventListener('DOMContentLoaded', function() {
     const contenedores = document.getElementById("reciclaje");       
     contenedores.addEventListener('click', function(){
@@ -19,5 +16,12 @@ document.addEventListener('DOMContentLoaded', function() {
     const contenedores = document.getElementById("curiosidades");       
     contenedores.addEventListener('click', function(){
         window.location.href="../Proyecto01/curiosidades/curiosidades.html";
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    const contenedores = document.getElementById("btn-login");       
+    contenedores.addEventListener('click', function(){
+        window.location.href="../Proyecto01/formulario/formularioLogin.html";
     });
 });
